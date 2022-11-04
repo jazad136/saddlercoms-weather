59ec49e427b1e6a3b1a706bf4c2fa51ba4b85ca9
jazad136 <1129384+jazad136@users.noreply.github.com>
2022-11-04 12:06:47 -0400

    Add Wind Speed Data to JSON Model

--- src/main/java/net/saddlercoms/weatherviewer/model/WindType.java
+++ src/main/java/net/saddlercoms/weatherviewer/model/WindType.java
@@ -1,5 +1,34 @@
 package net.saddlercoms.weatherviewer.model;
 
-public class WindType {
+import com.fasterxml.jackson.annotation.JsonProperty;
 
+public class WindType {
+	
+	/**  Wind speed. Unit: miles/hour. */
+	private Double speed;
+
+	/** Wind direction, degrees (meteorological) */
+	private Integer degrees;
+	
+	/** Wind gust.  Unit: miles/hour */
+	private Double gust;
+	
+	/** Wind speed.  Unit: miles/hour. */
+	@JsonProperty("speed") public Double getSpeed() { return speed; }
+	@JsonProperty("speed") public void setSpeed(Double speed) { this.speed = speed; }
+	
+	/** Wind direction, degrees (meteorological) */
+	@JsonProperty("deg") public Integer getDegrees() { return degrees; }
+	@JsonProperty("deg") public void setDegrees(Integer degrees) { this.degrees = degrees; }
+	
+	/** Wind gust. Unit: miles/hour */
+	@JsonProperty("gust") public Double getGust() { return gust; }
+	@JsonProperty("gust") public void setGust(Double gust) { this.gust = gust; }
+	
+	@Override
+	public String toString() {
+		return "Wind [speed=" + speed + ", degrees=" + degrees + ", gust=" + gust + "]";
+	}
+	
+	
 }
