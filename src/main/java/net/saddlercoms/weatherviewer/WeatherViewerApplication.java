4d39788806525d9e02519b3fa1297259d279c912
jazad136 <1129384+jazad136@users.noreply.github.com>
2022-11-09 18:22:08 -0500

    ServletInitializer is configured in Application

--- src/main/java/net/saddlercoms/weatherviewer/WeatherViewerApplication.java
+++ src/main/java/net/saddlercoms/weatherviewer/WeatherViewerApplication.java
@@ -2,9 +2,16 @@ package net.saddlercoms.weatherviewer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class WeatherViewerApplication {
+public class WeatherViewerApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(WeatherViewerApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(WeatherViewerApplication.class, args);
